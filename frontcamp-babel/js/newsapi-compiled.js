3f82c83016f34737f35ac19c8fd01d08f36d77fc
Aliaksandr Khvainitski <driv3r023@gmail.com>
2016-11-22 00:00:27 +0300

    [frontcamp-babel] Hometask for Babel - Part 2

--- frontcamp-babel/js/newsapi-compiled.js
+++ frontcamp-babel/js/newsapi-compiled.js
@@ -4,8 +4,6 @@ var _createClass = function () { function defineProperties(target, props) { for 
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-require("babel-polyfill");
-
 (function () {
     var NewsApi = function () {
         function NewsApi(sourceApi, keyApi, containerApi) {
