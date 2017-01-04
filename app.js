c14f493372e044443866f10f0c5034e5162eef10
Aliaksandr Khvainitski <aliaksandr_khvainitski@epam.com>
2017-01-04 13:45:47 +0300

    [frontcamp] Hometask for NodeJS session - Rest API

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var LocalStrategy = require('passport-local').Strategy;
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var posts = require('./routes/posts');
 
 var app = express();
 
@@ -67,6 +68,7 @@ app.use(function (req, res, next) {
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/posts', posts);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
